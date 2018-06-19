d8d84764392c4312df113abb81904f149ca344da
Sozzle <34127475+Sozzle@users.noreply.github.com>
2018-06-19 15:37:48 +0100

    Create Article.java

---
+++ Article.java
@@ -0,0 +1,43 @@
+public class Article {
+
+    //the title of the article/
+    private String mTitle;
+    //the name of the section/
+    private String mSection;
+    //the date of the article/
+    private String mDate;
+    //the author of the article/
+    private String mAuthor;
+
+/**construct a new (@link Article} object .
+ *
+ * @param title is the title of the article.
+ *
+ * @param section is the name of the section of the article.
+ *
+ * @param date is the date of the article.
+ *
+ * @param author is the author of the article.
+ */
+
+
+    public Article(String title, String section, String date, String author) {
+        mTitle = title;
+        mSection = section;
+        mDate = date;
+        mAuthor = author;
+    }
+
+    //returns the title of the article/
+    public String getTitle() {return mTitle;}
+
+    //returns the section of the article/
+    public String getSection() {return mSection;}
+
+    //returns the date of the article/
+    public String getDate() {return mDate;}
+
+    //returns the author of the article/
+    public String getAuthor() {return mAuthor;}
+
+}
