36f64465c78d71c522ef796cb00fbedaf4452c28
Sozzle <34127475+Sozzle@users.noreply.github.com>
2018-06-27 16:25:10 +0100

    Create ArticleLoader.java

---
+++ ArticleLoader.java
@@ -0,0 +1,58 @@
+package com.example.android.newsappstage1;
+
+import android.content.Context;
+import android.content.AsyncTaskLoader;
+import android.os.AsyncTask;
+
+import java.util.List;
+
+
+public class ArticleLoader extends AsyncTaskLoader<List<Article>> {
+
+
+
+            /**
+             * Tag for log messages
+             */
+    private static final String LOG_TAG = ArticleLoader.class.getName();
+
+
+    /**
+     * Constructs a new {@link ArticleLoader}.
+     *
+     * @param context of the activity
+     * @param url     to load data from
+     */
+    private String mUrl = "";
+
+    public ArticleLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+
+    }
+
+
+    @Override
+
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    /**
+     * * This is on a background thread.
+     */
+    @Override
+    public List<Article> loadInBackground() {
+        if (mUrl == null) {
+            return null;
+        }
+
+
+        List<Article> articles = (List<Article>) QueryUtils.fetchArticleData(mUrl);
+        return articles;
+    }
+}
+
+
+
+
